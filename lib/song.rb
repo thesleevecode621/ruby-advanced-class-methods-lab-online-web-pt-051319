04d86966c1a8c76db3e3ae62e9ac3cf04a50b2ab
boruch d <dubywider@gmail.com>
2019-05-28 16:22:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,7 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
- def self.create
-     song = Song.create
-Song.all.include?(song)
-    @@all <<
- end 
+ 
   def self.all
   @@all
   end

f3c1d0ad59207db9030bb41403fceaaa038dba6a
boruch d <dubywider@gmail.com>
2019-05-28 17:24:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,10 +32,7 @@ def self.find_or_create_by_name(song_name)
     self.find_by_name(song_name) || self.create_by_name(song_name)
   end
   
- def self.alphabetical
-    self.all.sort_by { |song| song.name }
-  end
- 
+
   def save
     self.class.all << self
   end

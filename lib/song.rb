4ed9ce8973f3147414cec3981e2d04204e0bb2c0
boruch d <dubywider@gmail.com>
2019-05-28 16:43:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,11 @@ class Song
     song.save
     song
   end 
-def self.new_by_name
+def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end  
   def self.all
   @@all
   end

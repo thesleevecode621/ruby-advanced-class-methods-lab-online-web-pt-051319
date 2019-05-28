8552bf36d2647fe745fd7f78369dbe28cba23b10
boruch d <dubywider@gmail.com>
2019-05-28 17:14:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,7 @@ def self.find_by_name(song_name)
     self.all.detect { |song| song.name == song_name }
   end
   def self find_or_create_by_name(name)
-self.find_by_name||self.create_by_name
+self.find_by_name(name)||self.create_by_name(name)
 end 
   def self.all
   @@all
